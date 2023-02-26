a166fa84be1e0af257f5b429194cd625fc823c0a
Satyam Tiwary <satyam@technoculture.io>
2023-02-26 20:34:43 +0530

    added a bunch of doctypes

---
+++ eln/eln/doctype/rodent_specimen/test_rodent_specimen.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Technoculture Research and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRodentSpecimen(FrappeTestCase):
+	pass
