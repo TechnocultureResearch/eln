91414590b7fa3b4b2a38768fcbec3677a1873fe8
Satyam Tiwary <satyam@technoculture.io>
2023-02-27 02:21:17 +0530

    fix: tailwind minor config

--- dashboard/tailwind.config.cjs
+++ dashboard/tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
