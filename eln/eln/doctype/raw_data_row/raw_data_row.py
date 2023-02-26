4eb99b3efd1a7d2e0f013eeded6be2de1bea52fb
Satyam Tiwary <satyam@technoculture.io>
2023-02-27 03:19:05 +0530

    ui: add tabs to reduce visual complexity

---
+++ eln/eln/doctype/raw_data_row/raw_data_row.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Technoculture Research and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class RawDataRow(Document):
+	pass
