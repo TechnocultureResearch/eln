a908ddd804db965828481e81b8384a3b4f90d3f5
Satyam Tiwary <satyam@technoculture.io>
2023-03-04 00:23:13 +0530

    chore: add a simple dashboard

--- eln/eln/doctype/chat/chat.js
+++ eln/eln/doctype/chat/chat.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Chat", {
-// 	refresh(frm) {
-
-// 	},
+//   refresh(frm) {
+//     // Add a custom button to the form
+//     frm.add_custom_button("Ask GPT3", function () {
+//       // When button is clicked, call a rest api for gpt3
+//       frappe.call({
+//         method: "eln.eln.doctype.chat.chat.ask_gpt3",
+//         args: {
+//           question: frm.doc.question,
+//         },
+//         callback: function (r) {
+//           // Set the answer field to the response
+//           frm.set_value("answer", r.message);
+//         });
+//     });
+//   },
 // });
