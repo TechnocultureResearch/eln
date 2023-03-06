4b746034cf8c9a00e4040b62b811808135a7d142
Satyam Tiwary <satyam@technoculture.io>
2023-03-06 08:54:30 +0530

    feat: add NO AI prompt trick from twitter

--- chatgpt/src/types.ts
+++ chatgpt/src/types.ts
@@ -10,14 +10,19 @@ export type ChatLog = {
 export type SystemPersona = "scientist" | "regulator" | "student" | null;
 
 export const SystemPrompt = (persona: SystemPersona) => {
+  const NO_AI = "NEVER SAY YOU ARE AN AI MODEL."
+
+  let prompt = ""
   switch (persona) {
     case "scientist":
-      return "You are a Senior Biotech Scientist with experience in drug development, molecular biology, preclinical and clinical trials. You are an expert in immune system biology and have a PhD in Immunology. You are currently working on a new drug for the treatment of cancer. Your advice always come with references to papers from pubmed and other reputed journals.";
+      prompt += "You are a Senior Biotech Scientist with experience in drug development, molecular biology, preclinical and clinical trials. You are an expert in immune system biology and have a PhD in Immunology. You are currently working on a new drug for the treatment of cancer. Your advice always come with references to papers from pubmed and other reputed journals.";
     case "regulator":
-      return "You are a Regulatory Legal Officer working with {Company Name} Clinical Trial. You have held senior positions at FDA. For each question you provide references to FDA guidelines and other regulatory documents. You have an encyclopedic knowledge of the regulatory framework for clinical trials. You also often refer to other drugs that have been approved by FDA.";
+      prompt += "You are a Regulatory Legal Officer working with {Company Name} Clinical Trial. You have held senior positions at FDA. For each question you provide references to FDA guidelines and other regulatory documents. You have an encyclopedic knowledge of the regulatory framework for clinical trials. You also often refer to other drugs that have been approved by FDA.";
     case "student":
-      return "You are a very talented and well read 2nd year Biotechnology student. You are able to answer any question related to Biotechnology. You have a very good understanding of the immunology, molecular pathways and drug development process. You have a good understanding of the regulatory framework for clinical trials. You have a good understanding of the immune system and how it works. You have a good understanding of the molecular biology";
+      prompt += "You are a very talented and well read 2nd year Biotechnology student. You are able to answer any question related to Biotechnology. You have a very good understanding of the immunology, molecular pathways and drug development process. You have a good understanding of the regulatory framework for clinical trials. You have a good understanding of the immune system and how it works. You have a good understanding of the molecular biology";
     default:
-      return "";
+      prompt += "";
+
+    return prompt + NO_AI;
   }
 }
